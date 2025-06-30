5edce04b3e948ea9023b4ea4207661758f8aa766
iqvato <106803452+iqvato@users.noreply.github.com>
2025-06-30 17:08:25 +0700

    Update 2.cpp

--- 2.cpp
+++ 2.cpp
@@ -1,26 +1,49 @@
 #include <iostream>
 #include <cmath>
 #include <iomanip>
-#include <vector>
+
+using namespace std;
 
 int main() {
-    const double g = 9.81;   
-    const double m = 75;  
-    const double k = 8;    
     
-    // Моменты времени 
-    const std::vector<double> time_points = {0, 0.25, 0.5, 0.75, 1, 2, 3, 4, 5};
+    //необходимые переменные
+    const double body_mass = 75.0;      
+    const double drag_coefficient = 8.0;    
+    const double gravity = 9.81; 
+    
+    //время, относительно которого считаем скорость
+    double time_points[] = {0, 0.25, 0.5, 0.75, 1, 2, 3, 4, 5}; 
+    int index = 0;
 
-    // Вычисление и вывод скорости для каждого t
-    std::cout << std::fixed << std::setprecision(3);
-    std::cout << "Время (с)\tСкорость (м/с)\n";
-    std::cout << "----------------------------\n";
 
-    for (double t : time_points) {
-        double v = sqrt(g * m / k) * tanh( t * sqrt(g * k / m));  
-        
-        std::cout << t << "\t\t" << v << "\n";
+    // Предусловие
+    cout << "Это работает по предусловию\n" ;
+    cout << "Время (с)\tСкорость (м/с)\n";
+    cout << "----------------------------\n";
+    while(index < 9) {
+        double current_time = time_points[index];
+        double velocity = sqrt(gravity * body_mass / drag_coefficient) * tanh(current_time * sqrt(gravity * drag_coefficient / body_mass));
+                         
+        cout << fixed << setprecision(3);
+        cout << current_time << "           " << velocity << endl;
+        index++;
     }
+
+    index = 0;
+    
+    //постусловие
+    cout << "А теперь с постусловием\n";
+    cout << "Время (с)\tСкорость (м/с)\n";
+    cout << "----------------------------\n";
+    do {
+        double current_time = time_points[index];
+        double velocity = sqrt(gravity * body_mass / drag_coefficient)
+                        * tanh(current_time * sqrt(gravity * drag_coefficient / body_mass));
+        
+        cout << fixed << setprecision(3);
+        cout << current_time << "           " << velocity << endl;
+        index++;
+    } while(index < 9);
 
     return 0;
 }
